0cfc1bcbd6e7d3396567da49de5c416ba76adcc0
git@github.com:Solly64/datasciencecoursera.git <git@github.com:Solly64/datasciencecoursera.git>
2019-07-15 00:14:40 -0400

    never give up

--- sports_predict/settings.py
+++ sports_predict/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'sports_predict.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'djangodbman',
+        'PASSWORD':'Oregon27!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
